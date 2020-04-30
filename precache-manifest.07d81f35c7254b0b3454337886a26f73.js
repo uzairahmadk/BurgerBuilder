c813f5e1d3f8654518ae98c9f74829beb6488368
uzairahmadk <59406979+uzairahmadk@users.noreply.github.com>
2020-04-30 23:45:06 +0500

    Add files via upload

---
+++ precache-manifest.07d81f35c7254b0b3454337886a26f73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b681a8e54ce95c794b1a61028a6ac1",
+    "url": "/MakeYourBurger/index.html"
+  },
+  {
+    "revision": "df99eb616ee94ffb0a8c",
+    "url": "/MakeYourBurger/static/css/3.b8110cba.chunk.css"
+  },
+  {
+    "revision": "c5e460ff16f7a763d560",
+    "url": "/MakeYourBurger/static/css/4.c9ba552a.chunk.css"
+  },
+  {
+    "revision": "5c29135ba81aa5875d53",
+    "url": "/MakeYourBurger/static/css/main.a8f2a67c.chunk.css"
+  },
+  {
+    "revision": "37c5c6fc3993577767e1",
+    "url": "/MakeYourBurger/static/js/2.3e25e2f8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/MakeYourBurger/static/js/2.3e25e2f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df99eb616ee94ffb0a8c",
+    "url": "/MakeYourBurger/static/js/3.0d23b675.chunk.js"
+  },
+  {
+    "revision": "c5e460ff16f7a763d560",
+    "url": "/MakeYourBurger/static/js/4.ef340efa.chunk.js"
+  },
+  {
+    "revision": "5c29135ba81aa5875d53",
+    "url": "/MakeYourBurger/static/js/main.50166afd.chunk.js"
+  },
+  {
+    "revision": "4df360f1bd0e5ca1447b",
+    "url": "/MakeYourBurger/static/js/runtime-main.f42aa2ac.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/MakeYourBurger/static/media/logo.b8503d26.png"
+  }
+]);
